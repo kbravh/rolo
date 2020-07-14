0e9cf07c5bccf43870d2d5cc87427926231264f7
Karey Higuera <karey.higuera@gmail.com>
2020-07-14 14:18:37 -0500

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Rolo</h1>
+    <div style={{ maxWidth: `600px`, marginBottom: `1.45rem` }}>
+      
+    </div>
+  </Layout>
+)
+
+export default IndexPage
